6f1218e1c18c6e57c440ce4e61c9e273ebe56cda
AlexanderHildreth <zander.hildreth@gmail.com>
2019-03-19 09:47:55 +0200

    Minor changes to update

--- search.php
+++ search.php
@@ -1,23 +1,24 @@
 <?php
 include_once 'config/database.php';
 include_once 'config/pagination.php';
-include_once 'controller/product.php';
+include_once 'config/partials.php';
+include_once 'controller/blog.php';
 include_once 'controller/category.php';
  
 $database = new Database();
 $db = $database->getConnection();
-$product = new Product($db);
+$blog = new Blog($db);
 $category = new Category($db);
 
 $search_term = isset($_GET['search']) ? $_GET['search'] : '';
 $page_title = "You searched for \"{$search_term}\"";
 
-include_once "views/partials/header.php";
+include_once $header;
 
-$stmt = $product->search($search_term, $from_record_num, $records_per_page);
+$stmt = $blog->search($search_term, $from_record_num, $records_per_page);
 
 $page_url="search.php?search={$search_term}&";
-$total_rows = $product->countAll_BySearch($search_term);
-include_once 'views/partials/read.php';
-include_once "views/partials/footer.php";
+$total_rows = $blog->countAll_BySearch($search_term);
+include_once 'views/read.php';
+include_once $footer;
 ?>
